8d476873d75462fa513369475ea2330453693a11
jyhong1 <wmbhoph1@gmail.com>
2022-03-05 01:18:22 +0900

    초기 커밋.

---
+++ assn1_test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+#include <vector>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+		glLineWidth(30.0);
+		glColor3f(1.0, 1.0, 1.0);
+		glVertex3f(1.0, -0.3, 0.0);
+		glVertex3f(-1.0, -0.3, 0.0);
+	glEnd();
+	glFlush();
+
+}
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(500, 320);
+
+	glutCreateWindow("fortress_test");
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
