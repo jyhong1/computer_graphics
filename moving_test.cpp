8732adde403d9732d008d36b15fff47ba987c4ea
Choi Junhyeok <cjh0630@postech.ac.kr>
2022-03-06 14:07:15 +0900

    renaming variables

--- moving_test.cpp
+++ moving_test.cpp
@@ -15,68 +15,68 @@ typedef struct rect {
 }rect;
 
 typedef struct circle {
-	float x;
-	float y;
-	float a;
+	float dx;
+	float r;
 }cir;
 
-rect rectangle;
-cir circle;
+rect lowerBody;
+cir upperBody;
 
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 0.0); //black
 	glShadeModel(GL_FLAT);
-	
-	rectangle.x = -0.3;
-	rectangle.y = -0.3;
-	rectangle.width = 0.3;
-	rectangle.height = 0.6;
 
-	circle.a = 0.0;
+	lowerBody.x = -0.3;
+	lowerBody.y = -0.3;
+	lowerBody.width = 0.3;
+	lowerBody.height = 0.6;
+
+	upperBody.dx = 0.0;
+	upperBody.r = 0.12;
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+
 	glBegin(GL_QUADS); //사각형
-		glColor3f(1.0, 1.0, 1.0); //white
-		glVertex2f(rectangle.x, rectangle.y);
-		glVertex2f(rectangle.x, rectangle.y + rectangle.width);
-		glVertex2f(rectangle.x + rectangle.height, rectangle.y + rectangle.width);
-		glVertex2f(rectangle.x + rectangle.height, rectangle.y);
+	glColor3f(1.0, 1.0, 1.0); //white
+	glVertex2f(lowerBody.x, lowerBody.y);
+	glVertex2f(lowerBody.x, lowerBody.y + lowerBody.width);
+	glVertex2f(lowerBody.x + lowerBody.height, lowerBody.y + lowerBody.width);
+	glVertex2f(lowerBody.x + lowerBody.height, lowerBody.y);
 	glEnd();
 
 	glBegin(GL_POLYGON); //https://blog.amaorche.com/25 circle
-		for (int i = 0; i < 360; i++) {
-			float angle = i * 3.141592 / 180;
-			circle.x = 0.12 * cos(angle);
-			circle.y = 0.12 * sin(angle);
-			glVertex2f(circle.x + circle.a, circle.y);
-		}
+	for (int i = 0; i < 360; i++) {
+		float angle = i * 3.141592 / 180;
+		float x = upperBody.r * cos(angle);
+		float y = upperBody.r * sin(angle);
+		glVertex2f(x + upperBody.dx, y);
+	}
 	glEnd();
 
-	glLineWidth(5.0); //begin 전에 선언해줘야함.
+	glLineWidth(5.0); //begin 전에 선언해줘야함. 왜?
 	glBegin(GL_LINES); // 땅(ground)
-		glColor3f(1.0, 0.0, 0.0); //red
-		glVertex3f(1.0, -0.3, 0.0);
-		glVertex3f(-1.0, -0.3, 0.0);
+	glColor3f(1.0, 0.0, 0.0); //red
+	glVertex3f(1.0, -0.3, 0.0);
+	glVertex3f(-1.0, -0.3, 0.0);
 	glEnd();
 
 
-	
+
 	glutSwapBuffers();
 }
 
 void specialkeyboard(int key, int x, int y) { //moving tank
 	switch (key) {
 	case GLUT_KEY_RIGHT:
-		rectangle.x += 0.01;
-		circle.a += 0.01;
+		lowerBody.x += 0.01;
+		upperBody.dx += 0.01;
 		break;
 
 	case GLUT_KEY_LEFT:
-		rectangle.x -= 0.01;
-		circle.a -= 0.01;
+		lowerBody.x -= 0.01;
+		upperBody.dx -= 0.01;
 		break;
 	}
 	glutPostRedisplay();
