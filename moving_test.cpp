9c4cfd0bc9c7464cb8cbe18ffa20d05c37630eaf
jyhong1 <wmbhoph1@gmail.com>
2022-03-09 21:01:55 +0900

    final(assn1)

--- moving_test.cpp
+++ moving_test.cpp
@@ -308,9 +308,9 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(700, 700);
+	glutInitWindowSize(700, 400);
 
-	glutCreateWindow("fortress_test");
+	glutCreateWindow("fortress_assn1");
 	init();
 
 	glutDisplayFunc(display);
